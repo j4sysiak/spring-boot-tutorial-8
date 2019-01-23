951ccf60d654cf9d01a153b4782a481f8e3679bc
j4sysiak <j4sysiak@gmail.com>
2019-01-23 20:26:09 +0100

    ss

---
+++ spring-boot-tutorial-8/src/main/java/com/caveofprogramming/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.caveofprogramming.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/rest")
+    public String rest() {
+        return "Greetings from Spring Boot!";
+    }
+    
+}
---
+++ spring-boot-tutorial-8/src/main/java/com/caveofprogramming/controllers/PageController.java
@@ -0,0 +1,32 @@
+package com.caveofprogramming.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+
+//	  @RequestMapping("/")
+//	  String index2() {
+//	    return "index2";
+//	  }
+	  
+	  @RequestMapping("/about")
+	  String about() {
+	    return "about";
+	  }
+	  
+	  @RequestMapping("/info")
+	  String info() {
+	    return "info";
+	  }
+    
+	  @RequestMapping("/hello")
+	  String hello() {
+	    return "hello";
+	  }
+	  
+}
